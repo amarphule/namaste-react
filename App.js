ff1db65fc0967ca014a68f2c463ccf1edf83f51e
amarphule <amardeepphule@gmail.com>
2023-08-28 15:19:34 +0530

    rendor using jsx

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
-//React create element without babel
-const heading = React.createElement('h1',{}, "Namaste React")
 
+// React element
+const description = <h3>We will be practicing React using parcel.</h3>
 
+// Reat Componet
+function Title(){
+    return <h1>Namaste React</h1>
+}
 
+// Root React component
+const App = ()=>{
+    return(
+        <div>
+            <Title/>
+            {description}
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading)
+root.render(<App/>)
